9893de5ce2fe4c0a95ca5cc051662d3f270846dc
Presto George <presto.george1@gmail.com>
2023-08-14 16:16:16 -0700

    initial pipeline

---
+++ bin/job_tracker.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { JobTrackerStack } from '../lib/job_tracker-stack';
+
+const app = new cdk.App();
+new JobTrackerStack(app, 'JobTrackerStack', {
+  env: {
+    account: '357200941310',
+    region: 'us-west-1',
+  }
+});
+
+app.synth();
---
+++ lib/job_tracker-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
+
+export class JobTrackerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'JobTrackerPipeline', {
+      pipelineName: 'JobTrackerPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.connection('prestogeorge/JobTracker', 'main', {
+          connectionArn: 'arn:aws:codestar-connections:us-west-1:357200941310:connection/e8ed2ad3-5d54-4d8e-9209-a41c40537924'
+        }),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
+  }
+}
