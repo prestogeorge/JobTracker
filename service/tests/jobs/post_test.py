55cf09680527d289eddba6b57f9109b070a58364
Presto George <presto.george1@gmail.com>
2023-08-16 15:31:51 -0700

    fixing unit test

--- service/tests/jobs/post_test.py
+++ service/tests/jobs/post_test.py
@@ -1,10 +1,20 @@
 import unittest
-
+import json
 from service.src.jobs.post import handler
 
 
 class PostJobTest(unittest.TestCase):
     def test_handler_returns_hello_world(self):
         actual = handler(None, None)
-        expected = "hello world"
+        expected = {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({
+                'success': True
+            }),
+            "isBase64Encoded": False
+        }
         self.assertEquals(actual, expected)
